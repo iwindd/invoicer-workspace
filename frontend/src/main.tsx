b323d645d3ba4eaa3b52716d729fd1dc49af3f89
iwindd <freewinendlife@gmail.com>
2024-05-24 19:18:45 +0700

    refactor: route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,13 @@
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import router from "./route";
 
 // providers
 import ThemeRegistry from "./styles/ThemeRegistry";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import QueryProvider from "./providers/QueryProvider";
-
-// routes
-import route from "./route";
-
-// routers
-const router = createBrowserRouter(
-  route.map((r) => ({ ...r, element: <r.element /> }))
-);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- frontend/src/route.tsx
+++ frontend/src/route.tsx
@@ -1,8 +1,13 @@
 // routes
+import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./routes/home";
+import SignInPage from "./routes/signin";
 
-const routes = [
-  { path: "/", element: HomePage }
-]
+const router = createBrowserRouter(
+  [
+    { path: "/", element: HomePage },
+    { path: "/signin", element: SignInPage },
+  ].map((r) => ({ ...r, element: <r.element /> }))
+);
 
-export default routes
+export default router;
