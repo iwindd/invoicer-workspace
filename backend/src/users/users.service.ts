a0f04b77c7210a2b552e18de1374f2cb6490505b
iwindd <freewinendlife@gmail.com>
2024-05-25 23:02:41 +0700

    perf: select scope

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -7,10 +7,17 @@ export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async findOne(email: string): Promise<User | undefined> {
-    return this.prisma.user.findFirst({
+    return (this.prisma.user.findFirst({
       where: {
         email: email,
       },
-    });
+      select: {
+        id: true,
+        email: true,
+        firstname: true,
+        lastname: true,
+        permission: true,
+      },
+    }) as unknown) as User | undefined;
   }
 }
