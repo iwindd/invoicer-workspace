2129b56686d170f0428e3f624ee5cf739051d316
iwindd <freewinendlife@gmail.com>
2024-05-26 02:15:27 +0700

    feat: table fetch

---
+++ backend/src/libs/table.ts
@@ -0,0 +1,107 @@
+import dayjs = require('dayjs');
+import { Filter, Pagination, SortDirection, SortItem } from './type';
+
+export const filter = (
+  filter: Filter,
+  rows: string[],
+  include?: (text: string) => any,
+) => {
+  try {
+    const text = filter?.quickFilterValues?.[0];
+    const items = filter?.items;
+    const currentTime = dayjs();
+
+    return {
+      OR: [
+        /* TEXT */
+        ...(text
+          ? [
+              ...rows.map((name: string) => ({
+                [`${name}`]: {
+                  contains: filter?.quickFilterValues?.[0],
+                  mode: 'insensitive',
+                },
+              })),
+              ...(include ? include(text) : []),
+            ]
+          : []),
+        /* ITEMS */
+        ...(items.length > 0
+          ? items.map(item => {
+              if (item.field == 'status') {
+                return {
+                  ['status']: {
+                    ...(item.value === -1
+                      ? { ['equals']: -1 }
+                      : item.value === 1
+                      ? { ['equals']: 1 }
+                      : item.value === 2
+                      ? { ['equals']: 2 }
+                      : { ['equals']: 0 }),
+                  },
+                  ...(item.value == 0
+                    ? {
+                        ['start']: {
+                          ['gte']: currentTime.toDate(),
+                        },
+                      }
+                    : item.value == 3
+                    ? {
+                        ['start']: { lte: currentTime.toDate() },
+                        ['end']: { gte: currentTime.toDate() },
+                      }
+                    : item.value == 4
+                    ? {
+                        ['end']: { lte: currentTime.toDate() },
+                      }
+                    : {}),
+                };
+              } else {
+                return {
+                  [`${item.field}`]: {
+                    [`${item.operator}`]: item.value,
+                  },
+                };
+              }
+            })
+          : []),
+      ],
+    };
+  } catch (error) {
+    return null;
+  }
+};
+
+export const order = (sort: SortItem[]): any => {
+  const orderBy: Record<any, SortDirection>[] = [
+    {
+      id: 'desc',
+    },
+  ];
+
+  try {
+    sort.map(sort => {
+      orderBy.unshift({
+        [sort.field]: sort.sort as SortDirection,
+      });
+    });
+
+    return orderBy;
+  } catch (error) {
+    return sort;
+  }
+};
+
+export const pagination = (pagination: Pagination) => {
+  try {
+    return {
+      take: Number(pagination.pageSize),
+      skip: Number(pagination.page * pagination.pageSize),
+    };
+  } catch (error) {
+    return {
+      take: 15,
+      skip: 0,
+    };
+  }
+};
---
+++ backend/src/libs/type.d.ts
@@ -0,0 +1,33 @@
+declare enum LogicOperator {
+  And = "and",
+  Or = "or"
+}
+export type SortDirection = 'asc' | 'desc' | null | undefined;
+export interface SortItem {
+  field: string;
+  sort: SortDirection;
+}
+
+export interface Pagination{
+  pageSize: number;
+  page: number;
+}
+
+export interface Filter{
+  items: {
+    id?: number | string;
+    field: string;
+    value?: any;
+    operator: string;
+  }[],
+  logicOperator?: LogicOperator;
+  quickFilterValues?: any[];
+  quickFilterLogicOperator?: LogicOperator;
+  quickFilterExcludeHiddenColumns?: boolean;
+}
+
+export interface TableFetch{
+  sort: SortItem[],
+  pagination: Pagination,
+  filter: Filter
+}
