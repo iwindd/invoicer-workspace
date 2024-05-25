ae23c83050c47699eb82d4051fd3c45799cffba7
iwindd <freewinendlife@gmail.com>
2024-05-25 22:27:00 +0700

    feat: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.CORS_ORIGIN_URL, credentials: true });
-  app.setGlobalPrefix('api');
-
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+  app.setGlobalPrefix("api");
+  
   await app.listen(3000);
 }
 bootstrap();
