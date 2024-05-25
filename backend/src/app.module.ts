7e22b741e4c60c2c837bec9a9677fe2e98e3059f
iwindd <freewinendlife@gmail.com>
2024-05-26 00:59:01 +0700

    feat: customers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, CustomersModule],
   controllers: [],
   providers: [],
 })
