a828dce4983581ccbbd3c00cb71192baca087930
iwindd <freewinendlife@gmail.com>
2024-05-28 22:12:59 +0700

    feat: streaming file

--- backend/src/notice/notice.controller.ts
+++ backend/src/notice/notice.controller.ts
@@ -5,6 +5,16 @@ import { NoticeService } from './notice.service';
 export class NoticeController {
   constructor(private readonly noticeService: NoticeService) {};
 
+  @HttpCode(200)
+  @Get('images/:name')
+  getFile(@Param("name") name: string, @Res() res : any){
+    try {
+      res.sendFile(join(process.cwd(), `uploads/${name}`))
+    } catch (error) {
+      throw new BadRequestException();
+    }
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.noticeService.fineOne(+id);
