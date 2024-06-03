c525f4a807636ee2b98ae91772ddc899da9a3172
iwindd <freewinendlife@gmail.com>
2024-06-03 18:08:11 +0700

    refactor: queryClient

--- frontend/src/providers/QueryProvider.tsx
+++ frontend/src/providers/QueryProvider.tsx
@@ -1,23 +1,21 @@
-import { useEffect, useState } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchInterval: 5 * 60 * 1000,
+      retry: 1,
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+    },
+  },
+});
+
 
 export default function QueryProvider({ children }: {
   children: React.ReactNode
 }) {
-  const [queryClient] = useState(() => new QueryClient());
-
-  useEffect(() => {
-    queryClient.setDefaultOptions({
-      queries: {
-        refetchOnWindowFocus: false,
-        refetchOnMount: false,
-        refetchInterval: 5 * 60 * 1000,
-        retry: 1,
-        retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
-      },
-    })
-  })
-
   return (
     <QueryClientProvider client={queryClient}>
       {children}
